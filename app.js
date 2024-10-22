ef717c06b7dd2e9964c67806ca4bd47d914dc6db
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-22 16:00:17 +0200

    ADD bonus | shopping list FIXED div class name

--- app.js
+++ app.js
@@ -20,3 +20,43 @@ function changeLampStatus(){
 
 lightLamp.addEventListener('click', changeLampStatus);
 
+
+//BONUS-2 Stampa in una ul un array di stringhe dato
+
+let shoppingList= document.getElementById("shopping_list");
+//console.log(shoppingList)
+
+let listItems = ["pane","pizza","pasta","lattuga","pomodori"];
+
+const addProductButton = document.getElementById("add_product");
+
+const productInput = document.getElementById("product");
+let product = productInput.value;
+
+function displayList(container="shoppingList", list){
+    for(let i=0; i< listItems.length; i++)
+        {
+            let listItem=document.createElement("li");
+            listItem.innerText=listItems[i];
+            shoppingList.appendChild(listItem);
+        }
+}
+displayList(shoppingList,listItems);
+
+function addProduct(container,list, item){
+    list.push(item);
+    //console.log(list[list.length-1])
+    let listItem=document.createElement("li");
+    listItem.innerText=list[list.length-1]
+    container.appendChild(listItem);
+}
+
+addProductButton.addEventListener("click", function(){
+    const productInput = document.getElementById("product");
+    let product = productInput.value;
+    //console.log(product)
+    addProduct(shoppingList,listItems,product);
+});
+
+
+
