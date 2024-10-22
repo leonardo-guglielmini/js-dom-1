32cebe49c462ba67faf301bab4a071f573b3f6cd
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-22 13:55:53 +0200

    ADD lampOn, lampOff, lightLamp event listener, changeLampStatus function,

---
+++ app.js
@@ -0,0 +1,23 @@
+const lampOn = document.getElementById("lamp_on");
+const lampOff = document.getElementById("lamp_off");
+const lightLamp =  document.getElementById("button");
+
+function changeLampStatus(){
+    if(lightLamp.classList.contains("off")){
+        lampOff.classList.add("hidden");
+        lampOn.classList.remove("hidden");
+        lightLamp.classList.add("on");
+        lightLamp.classList.remove("off");
+        lightLamp.innerText="Spegni";
+    }else{
+        lampOn.classList.add("hidden");
+        lampOff.classList.remove("hidden");
+        lightLamp.classList.add("off");
+        lightLamp.classList.remove("on");
+        lightLamp.innerText="Accendi";
+    }
+    
+}
+
+lightLamp.addEventListener('click', changeLampStatus);
+
